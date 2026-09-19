b31bfe0d6791612c2321f1e28abef99868376669
danikrakatau1 <danikrakatau1@gmail.com>
2026-09-20 02:22:07 +0700

    revert(gallery): remove BODY transparency experiment

--- assets/js/public-source-truth-renderer-v1.js
+++ assets/js/public-source-truth-renderer-v1.js
@@ -547,7 +547,6 @@
       if(galleryPerf.count){
         blocks.push('<script src="'+localAsset('/assets/js/gallery-performance-v1.js?v=141')+'"></script>');
         blocks.push('<script src="'+localAsset('/assets/js/gallery-breathing-v1.js?v=130')+'"></script>');
-        blocks.push('<script src="'+localAsset('/assets/js/gallery-section-transparent-v1.js?v=300')+'"></script>');
       }
     }else{
       if(pkg.authority){
