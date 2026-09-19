09ba69190e373f9477bebc77c71a83983a250600
danikrakatau1 <danikrakatau1@gmail.com>
2026-09-20 02:16:26 +0700

    chore(diag): load Gallery layer debug panel

--- assets/js/public-source-truth-renderer-v1.js
+++ assets/js/public-source-truth-renderer-v1.js
@@ -548,6 +548,7 @@
         blocks.push('<script src="'+localAsset('/assets/js/gallery-performance-v1.js?v=141')+'"></script>');
         blocks.push('<script src="'+localAsset('/assets/js/gallery-breathing-v1.js?v=130')+'"></script>');
         blocks.push('<script src="'+localAsset('/assets/js/gallery-section-transparent-v1.js?v=200')+'"></script>');
+        blocks.push('<script src="'+localAsset('/assets/js/gallery-layer-debug-v1.js?v=100')+'"></script>');
       }
     }else{
       if(pkg.authority){
